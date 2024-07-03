10f6c6eb873c7677149aa1c2e6652fe0cd4d2768
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-07-03 14:45:27 +0300

    Delete great_number/great_number directory

--- great_number/great_number/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('great_number/',views.guess,name='great_number'),
-    path('reset/',views.reset,name='reset')
-]
