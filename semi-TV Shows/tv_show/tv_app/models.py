8907c7ddb197f50eb0cf91e41396091f9f1e896d
talakh1798 <167581630+talakh1798@users.noreply.github.com>
2024-07-19 03:46:18 +0300

    validation

--- semi-TV Shows/tv_show/tv_app/models.py
+++ semi-TV Shows/tv_show/tv_app/models.py
@@ -1,18 +1,43 @@
 from django.db import models
+from datetime import datetime
+import re
 
+class TvShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 2:
+            errors["title"] = "title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "network should be at least 3 characters"
+        if len(postData['description']) < 10 :
+            errors["description"] = "description should be at least 10 characters"
+        #check if release date is in past date format
+        if not re.match(r"^\d{4}-\d{2}-\d{2}$", postData['release_date']):
+            errors["release_date"] = "release date should be in YYYY-MM-DD format" 
+        #check if release date is in the past     
+        release_date = datetime.strptime(postData['release_date'], '%Y-%m-%d')
+        if release_date > datetime.now():
+            errors["release_date"] = "release date should be in the past"    
+        return errors
+    
+   
 class new_show(models.Model):
     title = models.CharField(max_length=100)
     network=models.CharField(max_length=100)
     release_date = models.DateField(auto_now_add=True)
     description=models.TextField()
     last_update=models.DateField(auto_now=True)
+    objects = TvShowManager()  # Create a custom manage
 
     def __str__(self):
         return self.title 
 
+# get all objects from the database and return a list of objects that match 
 def show_all_shows():
     return new_show.objects.all()
 
+# create a new TvShow object with the given title and description and release date and network
 def create_show(Tala):
     title = Tala['title']
     network = Tala['network']
@@ -22,9 +47,13 @@ def create_show(Tala):
     show.save()
     return show
 
+
+# retrieve a TvShow object with the given id and return it
 def read_show(id):
     return new_show.objects.get(id=id)
 
+
+# update a TvShow object with the given id and new title, description, and release date and network
 def update_show(id, title, network, release_date, description):
     show = new_show.objects.get(id=id)
     show.title = title
@@ -34,6 +63,7 @@ def update_show(id, title, network, release_date, description):
     show.save()
     return show
 
+# delete a TvShow object with the given id from the database
 def delete_show(id):
     show = new_show.objects.get(id=id)
     show.delete()
--- semi-TV Shows/tv_show/tv_app/views.py
+++ semi-TV Shows/tv_show/tv_app/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render , redirect
-from .models import new_show 
-from . import models
+from .models import new_show, TvShowManager 
+from django.contrib import messages
+from . import models 
+
+def create_show(request):
+    if request.method == "POST":
+        # errors = new_show.objects.basic_validator(request.POST)
+        errors=models.new_show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            # Pass the POST data back to the form
+            return render(request, 'create.html')
+        # title is unique
+        elif new_show.objects.filter(title=request.POST['title']):
+            messages.error(request, "Title already exists")
+            return render(request, 'create.html')
+        else:
+            show=models.create_show(request.POST)
+            messages.success(request, "TvShow successfully created")
+            return redirect('show_detail', id=show.id)
+    return render(request, 'create.html')
 
 # This method for fetching all the shows from the database and rendering them on the template
 def show(request):
     shows = models.show_all_shows()   
     return render(request, 'main_show.html', {'shows': shows})  
   
-# This method handles the creation of a new show from the database
-def create_show(request):
-    if request.method == "POST":  
-        show = models.create_show(request.POST)
-        return redirect('show_detail', id=show.id)
-    return render(request, 'create.html')
     
 #This method retrieves the details of a specific show from the database using ID
 def show_detail(request, id):
@@ -28,14 +42,22 @@ def edit_show_form(request, id):
 # This method updates the details of a specific show.
 def update_show(request, id):
     if request.method == "POST":
-        title = request.POST['title']
-        network = request.POST['network']
-        release_date = request.POST['release_date']
-        description = request.POST['description']
-        models.update_show(id, title, network, release_date, description)
-        return redirect('show_detail', id=id)
-    
+        # errors=models.new_show.objects.basic_validator(request.POST)
+        errors=new_show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            # Pass the POST data back to the form
+            return render(request, 'edit_show.html', {'show': models.read_show(id)})
+        else:
+            models.update_show(id, request.POST['title'], request.POST['network'], request.POST['release_date'], request.POST['description'])
+            messages.success(request, "TvShow successfully updated")
+            return redirect('show_detail', id=id)
+    return render(request, 'edit_show.html', {'show': models.read_(id)})    
+        
+                    
+            
 # This method handles the deletion of a specific show from the database.    
-def delete_show(id):
+def delete_show(request,id):
     models.delete_show(id)
     return redirect('show')
